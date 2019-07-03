b197d32587d2f32ab44231d057189e2d7db74136
Femke <femke.bruckmann@ugent.be>
2019-07-03 10:03:28 +0200

    Canvas now shows up on the index page

--- prototype/src/App.vue
+++ prototype/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Canvas></Canvas>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Canvas
   }
 }
 </script>
