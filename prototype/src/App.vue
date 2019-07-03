a6db1c41e887a4c64a1c40b777eff8a686894554
Femke <femke.bruckmann@ugent.be>
2019-07-03 12:58:16 +0200

    Add shape on button push

--- prototype/src/App.vue
+++ prototype/src/App.vue
@@ -1,21 +1,17 @@
 <template>
     <div id="app">
-        <!--        <img alt="Vue logo" src="./assets/logo.png">-->
-        <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
         <Canvas></Canvas>
     </div>
 </template>
 
 <script>
-    // import HelloWorld from './components/HelloWorld.vue'
     import Canvas from './components/Canvas.vue'
 
     export default {
         name: 'app',
         components: {
-            // HelloWorld,
-            Canvas
-        }
+            Canvas,
+        },
     }
 </script>
 
--- prototype/src/components/Canvas.vue
+++ prototype/src/components/Canvas.vue
@@ -1,14 +1,18 @@
 <template>
-    <div id="container" style="background-color: lightgrey;">
-        <v-stage :config="configKonva" id="canvas">
+    <div>
+        <v-stage ref="stage" :config="configKonva" id="canvas" style="background-color: lightgrey;">
             <v-layer>
                 <v-circle :config="configCircle"></v-circle>
             </v-layer>
         </v-stage>
+        <ShapeAdder @add-shape="createShape()"></ShapeAdder>
     </div>
 </template>
 
 <script>
+    import ShapeAdder from './ShapeAdder.vue';
+    import Konva from 'konva';
+
     const width = 0.9 * window.innerWidth;
     const height = 0.9 * window.innerHeight;
 
@@ -18,7 +22,6 @@
                 configKonva: {
                     width: width,
                     height: height,
-                    container: 'container',
                 },
                 configCircle: {
                     x: 100,
@@ -29,6 +32,27 @@
                     strokeWidth: 4
                 }
             };
+        },
+        methods: {
+            createShape: function () {
+                const stage = this.$refs.stage.getNode();
+                let layer = new Konva.Layer();
+                let item = new Konva.Circle({
+                    x: 200,
+                    y: 100,
+                    radius: 70,
+                    fill: "red",
+                    stroke: "black",
+                    strokeWidth: 4,
+                    draggable: true,
+                });
+                layer.add(item);
+                stage.add(layer);
+                stage.draw();
+            }
+        },
+        components: {
+            ShapeAdder
         }
     };
 </script>
