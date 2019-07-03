a4fe20e5c084400175c3f65093decbfd80137b76
Femke <femke.bruckmann@ugent.be>
2019-07-03 09:46:39 +0200

    Import Konva

---
+++ prototype/src/components/Canvas.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-stage :config="configKonva">
+        <v-layer>
+            <v-circle :config="configCircle"></v-circle>
+        </v-layer>
+    </v-stage>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                configKonva: {
+                    width: 200,
+                    height: 200
+                },
+                configCircle: {
+                    x: 100,
+                    y: 100,
+                    radius: 70,
+                    fill: "red",
+                    stroke: "black",
+                    strokeWidth: 4
+                }
+            };
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
